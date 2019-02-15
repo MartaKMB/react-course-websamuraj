2df02ccd00fb6c156f7dac081bcd47b2528ec554
MartaMucha <marta.mucha.balcerek@rsqtechnologies.com>
2019-02-15 17:16:54 +0100

    first attempt on course to cra

---
+++ cra-stoper/src/components/SwitchButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SwitchButton = props => (
+    <button onClick={props.click}>
+        { props.active
+            ? "stop" 
+            : "start"
+        }
+    </button>
+)
+
+export default SwitchButton;
