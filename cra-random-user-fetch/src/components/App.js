dccc4546d5528dcdc6ae69d6b3225839138c40fd
MartaMucha <marta.mucha.balcerek@rsqtechnologies.com>
2019-03-01 08:52:06 +0100

    added cra-random-user-fetch app

---
+++ cra-random-user-fetch/src/components/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import UsersList from './UsersList';
+import ButtonFetchUsers from './ButtonFetchUsers';
+
+const API = 'https://randomuser.me/api/?results=5';
+
+class App extends Component {
+  state = {
+    users: null
+  }
+
+  handleDataFetch = () => {
+    fetch(API)
+      .then(response => {
+        if(response.ok) {
+          return response;
+        }
+        throw Error(response.status)
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          users: data.results
+        })
+      })
+      .catch(error => console.log('błąd: ', error));
+  }
+
+  render() {
+    const users = this.state.users;
+    return (
+      <div>
+        <ButtonFetchUsers click={this.handleDataFetch} />
+        {users ? <UsersList users={users} /> : users}
+      </div>
+    );
+  }
+}
+
+export default App;
